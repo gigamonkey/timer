612337cfcfab055d953d57a622832c7e2d420f13
Peter Seibel <peter@gigamonkeys.com>
2023-07-11 21:14:14 -0700

    Add left and right shift.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const $ = (q) => document.querySelector(q);
 
 let time = 0;
+let timer;
 
 const fmt = (t) => {
   const minutes = Math.floor(t / 60);
@@ -11,27 +12,62 @@ const fmt = (t) => {
   return `${mm}:${ss}`;
 };
 
-const makeClicker = (b, amt) => {
-  return (e) => {
-    if (b.classList.contains('selected')) {
-      b.classList.remove('selected');
-      time -= amt;
+const countdown = () => {
+  const start = Date.now();
+  const end = Date.now() + time * 1000;
+
+  const update = () => {
+    const now = Date.now();
+    if (now < end) {
+      $('#clock').innerText = fmt(Math.round((end - now) / 1000))
+      setTimeout(update, 1000);
     } else {
-      b.classList.add('selected');
-      time += amt
+      $('#clock').innerText = fmt(0);
     }
-    $('#clock').innerText = fmt(time);
-  };
+  }
+  update();
+};
+
+const changeTime = (fn) => {
+  time = fn(time);
+  $('#clock').innerText = fmt(time);
+
+  let left = time;
+  $('#buttons').querySelectorAll('button').forEach(b => {
+    const s = Number(b.dataset.seconds);
+    if (s <= left) {
+      b.classList.add('selected');
+      left -= s;
+    } else {
+      b.classList.remove('selected');
+    }
+  });
 }
+
+const buttonHandler = (e) => {
+  if (e.target.classList.contains('selected')) {
+    changeTime(t => t - Number(e.target.dataset.seconds));
+  } else {
+    changeTime(t => t + Number(e.target.dataset.seconds));
+  }
+};
 
 for (let i = 0; i < 6; i++) {
   const b = document.createElement('button');
-  b.onclick = makeClicker(b, 2 ** i * 60);
+  b.dataset.seconds = 2 ** i * 60;
+  b.onclick = buttonHandler;
   $('#buttons').prepend(b)
 }
+
 $('#buttons').append(document.createTextNode(':'));
-for (let i = 0; i < 6; i++) {
+
+for (let i = 0; i < 2; i++) {
   const b = document.createElement('button');
-  b.onclick = makeClicker(b, 2 ** (-1 - i) * 60);
+  b.dataset.seconds = 2 ** (-1 - i) * 60;
+  b.onclick = buttonHandler;
   $('#buttons').append(b)
 }
+
+$('#left').onclick = () => changeTime(t => t * 2);
+$('#right').onclick = () => changeTime(t => t / 2);
+$('#clock').onclick = countdown;
